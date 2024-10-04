511b73fb2ed270b4f97e55159ac4c9ed5dc48f71
nelasunitha <80846358+nelasunitha@users.noreply.github.com>
2024-10-03 22:49:43 -0700

    Adds functionality to shuffle_deck

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♣"]
 
@@ -6,4 +7,17 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 # Implement a solution for shuffling a deck of cards without using random.shuffle()
 # Using randint() is ok
 def shuffle_deck(deck_of_cards):
-    pass
+
+    l = len(deck_of_cards)
+    # iterating from the last index
+    for i in range(l-1, 0, -1):
+        j = random.randint(0, i)
+
+        # swapping the cards starting from last index to random index
+        deck_of_cards[i], deck_of_cards[j] = deck_of_cards[j], deck_of_cards[i]
+    return deck_of_cards
+
+deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
+
+shuffle_deck = shuffle_deck(deck_of_cards)
+print(f"After shuffling: {shuffle_deck}")
